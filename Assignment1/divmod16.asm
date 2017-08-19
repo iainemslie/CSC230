929699896e307d0d36dacd0fac76e948b91f690d
Iain Emslie <iainemslie@gmail.com>
2017-08-18 23:43:42 -0700

    Add files via upload

---
+++ Assignment1/divmod16.asm
@@ -0,0 +1,82 @@
+; divmod16.asm
+; CSC 230 - Summer 2017
+;
+; Starter code for assignment 1
+;
+; B. Bird - 04/30/2017
+
+.cseg
+.org 0
+
+	; Initialization code
+	; Do not move or change these instructions or the registers they refer to. 
+	; You may change the data values being loaded.
+	; The default values set A = 0x3412 and B = 0x0003
+	ldi r16, 0x0F ;Low byte of operand A
+	ldi r17, 0x7C ;High byte of operand A
+	ldi r18, 0xEC ;Low byte of operand B
+	ldi r19, 0x72 ;High byte of operand B
+	
+	; Your task: Perform the integer division operation A/B and store the result in data memory. 
+	; Store the 2 byte quotient in DIV1:DIV0 and store the 2 byte remainder in MOD1:MOD0.
+
+;Set labels for registers
+.def A_LOW = r25
+.def A_HIGH = r24
+.def B_LOW = r27
+.def B_HIGH = r26
+.def COUNTER_HIGH = r28	
+.def COUNTER_LOW = r29
+.def COUNTER_ADD = r20
+.def COUNTER_SUB = r21
+.def ZERO = r22
+
+;Move initial values to named registers
+	mov A_LOW, r16	;Low byte of A
+	mov A_HIGH, r17	;High byte of A
+	mov B_LOW, r18	;Low byte of B
+	mov B_HIGH, r19	;High byte of B
+	ldi COUNTER_ADD, 0x01 ;Value to add to counter each time
+	ldi ZERO, 0x00
+
+;Subtract A from B
+loop:
+	sub A_LOW, B_LOW
+	sbc A_HIGH, B_HIGH
+	rjmp negative_reached
+back:
+	rjmp inc_counter
+	rjmp loop
+
+;Increase counter each time the loop iterates
+inc_counter:
+	add COUNTER_LOW, COUNTER_ADD
+	adc COUNTER_HIGH, ZERO
+	rjmp loop
+
+negative_reached:
+	brcc back
+	add A_LOW, r18
+	adc A_HIGH, r19
+
+;Copy quotient and remained to data memory
+copy_to_data:
+	sts DIV0, COUNTER_LOW
+	sts DIV1, COUNTER_HIGH
+	sts MOD0, A_LOW
+	sts MOD1, A_HIGH
+	
+
+	; End of program (do not change the next two lines)
+stop:
+	rjmp stop
+	
+; Do not move or modify any code below this line. You may add extra variables if needed.
+; The .dseg directive indicates that the following directives should apply to data memory
+.dseg 
+.org 0x200 ; Start assembling at address 0x200 of data memory (addresses less than 0x200 refer to registers and ports)
+
+DIV0:	.byte 1 ; Bits  7...0 of the quotient
+DIV1:	.byte 1 ; Bits 15...8 of the quotient
+MOD0:	.byte 1 ; Bits  7...0 of the remainder
+MOD1:	.byte 1 ; Bits 15...8 of the remainder
---
+++ Assignment1/mul32.asm
@@ -0,0 +1,93 @@
+; mul32.asm
+; CSC 230 - Summer 2017
+;
+; Starter code for assignment 1
+;
+; B. Bird - 04/30/2017
+
+.cseg
+.org 0
+	; Initialization code
+	; Do not move or change these instructions or the registers they refer to. 
+	; You may change the data values being loaded.
+	; The default values set A = 0x3412 and B = 0x2010
+	ldi r16, 0xFF ;Low byte of operand A
+	ldi r17, 0xFF;High byte of operand A
+	ldi r18, 0xFF ;Low byte of operand B
+	ldi r19, 0xFF ;High byte of operand B
+
+	;Set everything to zero
+	ldi r20, 0x00	
+	ldi r21, 0x00 	
+	ldi r22, 0x00 	
+	ldi r23, 0x00  
+	ldi r24, 0x00   ;Set to zero empty slot to add with carry
+	ldi r25, 0x00   ;Use for loop counter low byte
+	ldi r26, 0x00	;Use for loop counter high byte
+	ldi r28, 0x01   ;Use as flag for counter
+
+
+	mov r26, r18 ; Store value of low byte B in counter
+	mov r25, r19 ; Store value of high byte B in counter
+
+loop:
+	add r23, r16  ; Add low byte1 to itself
+	adc r22, r17  ; Add high byte1 to itself (Order for readability)
+	adc r21, r24  ; For overflow
+	adc r20, r24  ; For overflow
+
+	tst r28
+	brne DecLow   ;If r28 has flag set then decrement low register
+	tst r26	  	  ; Compare low byte of counter to zero
+	brne DecLow  ; If High value is not zero then decrement
+	
+rjmp loop
+
+DecHigh:
+	dec r25
+	ldi r28, 0x01 ;Set to 0x01 if high bit decremented
+	rjmp loop
+
+DecLow:
+	ldi r28, 0x00
+	dec r26
+	tst r25	  ; Compare high counter to zero 
+	breq test_counter	  ; If high counter reaches zero then exit
+	tst r26	  	  ; Compare low byte of counter to zero
+	breq DecHigh   ; Otherwise decrement high value
+	
+
+	rjmp loop
+	
+	
+	; Your task: compute the 32-bit product A*B (using the bytes from registers r16 - r19 above as the values of
+	; A and B) and store the result in the locations OUT3:OUT0 in data memory (see below).
+	; You are encouraged to use a simple loop with repeated addition, not the MUL instructions, although you are
+	; welcome to use MUL instructions if you want a challenge.
+
+test_counter:
+	tst r26 ;If reached here and both counter bytes are zero then break out 
+	breq add_to_mem
+	rjmp loop ;If both counters are not zero then return to loop
+	
+add_to_mem:
+	STS OUT0,r23	
+	STS OUT1,r22
+	STS OUT2,r21
+	STS OUT3,r20
+
+	
+	; End of program (do not change the next two lines)
+stop:
+	rjmp stop
+
+	
+; Do not move or modify any code below this line. You may add extra variables if needed.
+; The .dseg directive indicates that the following directives should apply to data memory
+.dseg 
+.org 0x200 ; Start assembling at address 0x200 of data memory (addresses less than 0x200 refer to registers and ports)
+
+OUT0:	.byte 1 ; Bits  7...0 of the output value
+OUT1:	.byte 1 ; Bits 15...8 of the output value
+OUT2:	.byte 1 ; Bits 23...16 of the output value
+OUT3:	.byte 1 ; Bits 31...24 of the output value
